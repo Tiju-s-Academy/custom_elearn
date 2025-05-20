7042b34b30addadc1f7e99f8d6227befe1a3917b
Muhammed Anas <muhammadanaswayanad@gmail.com>
2025-05-20 14:05:47 +0530

    Refactor survey match following templates; remove unused assets and enhance drag-and-drop functionality with JavaScript initialization.

--- __manifest__.py
+++ __manifest__.py
@@ -11,11 +11,6 @@
         'views/survey_question_inherit_view.xml',
         'views/survey_match_templates.xml',
     ],
-    'assets': {
-        'web.assets_common': [
-            'custom_elearn/static/src/js/survey_match_following.js',
-        ],
-    },
     'license': 'LGPL-3',
     'application': True,
 }
